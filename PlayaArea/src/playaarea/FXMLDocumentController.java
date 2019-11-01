e4310a9db9c273b6af7d01f0bbc1c7dc81e5360b
Chokerino <43617111+Chokerino@users.noreply.github.com>
2019-11-01 21:01:59 +0530

    lawnmower

--- PlayaArea/src/playaarea/FXMLDocumentController.java
+++ PlayaArea/src/playaarea/FXMLDocumentController.java
@@ -23,7 +23,7 @@ import javafx.util.Duration;
  * @author saad
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private ImageView z1;
     
@@ -38,23 +38,74 @@ public class FXMLDocumentController implements Initializable {
     private ImageView lm4;
     @FXML
     private ImageView lm5;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         TranslateTransition tt = new TranslateTransition();
-        tt.setDuration(Duration.seconds(100));
-        tt.setToX(-750);
-        tt.setNode(z1);
-        tt.play();
         
         lm1.setOnMouseClicked(new EventHandler<MouseEvent>(){
  
             @Override
             public void handle(MouseEvent event) {
                 TranslateTransition tt2 = new TranslateTransition();
-                tt2.setDuration(Duration.seconds(15));
+                tt2.setDuration(Duration.seconds(2));
                 tt2.setToX(1000);
                 tt2.setNode(lm1);
+                tt2.play();
+            }
+        });
+        lm2.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                TranslateTransition tt2 = new TranslateTransition();
+                tt2.setDuration(Duration.seconds(2));
+                tt2.setToX(1000);
+                tt2.setNode(lm2);
+                tt2.play();
+            }
+        });
+        lm3.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                TranslateTransition tt2 = new TranslateTransition();
+                tt2.setDuration(Duration.seconds(2));
+                tt2.setToX(1000);
+                tt2.setNode(lm3);
+                tt2.play();
+            }
+        });
+        lm4.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                TranslateTransition tt2 = new TranslateTransition();
+                tt2.setDuration(Duration.seconds(2));
+                tt2.setToX(1000);
+                tt2.setNode(lm4);
+                tt2.play();
+            }
+        });
+        lm5.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                TranslateTransition tt2 = new TranslateTransition();
+                tt2.setDuration(Duration.seconds(2));
+                tt2.setToX(1000);
+                tt2.setNode(lm5);
+                tt2.play();
+            }
+        });
+        z1.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                TranslateTransition tt2 = new TranslateTransition();
+                tt2.setDuration(Duration.seconds(7));
+                tt2.setToX(-750);
+                tt2.setNode(z1);
                 tt2.play();
             }
         });
--- PlayaArea/src/playaarea/PlayaArea.java
+++ PlayaArea/src/playaarea/PlayaArea.java
@@ -20,11 +20,12 @@ public class PlayaArea extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
+
     }
 
     /**
